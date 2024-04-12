e127b3b7639988d64252f82d008e325e74d6b0c6
Welder Marcos <weldermarcosxd@gmail.com>
2024-04-12 15:31:25 -0300

    app nextjs

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+// imports
+import NextAuth from "next-auth";
+
+import GithubProvider from "next-auth/providers/github";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+const handler = NextAuth({
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_ID as string,
+      clientSecret: process.env.KEYCLOAK_SECRET as string,
+      issuer: process.env.KEYCLOAK_ISSUER,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
